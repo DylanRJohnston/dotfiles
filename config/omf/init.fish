2ff932e5972c2553d10704523cd796691951c8f2
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2016-11-08 14:22:57 +0800

    Improved cd alias. Now supports directory history

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -14,14 +14,51 @@ end
 # UTILITY FUNCTIONS
 #
 
-# Use Z instead of cd
-function cd
+function cd --description 'Change directory'
+    set -l MAX_DIR_HIST 25
+
+    if test (count $argv) -gt 1
+        printf "%s\n" (_ "Too many args for cd command")
+        return 1
+    end
+
+    # Skip history in subshells.
+    if status --is-command-substitution
+        builtin cd $argv
+        return $status
+    end
+
+    # Avoid set completions
+    set -l previous $PWD
+
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+
+    set -l cd_status
     if test -d "$argv"
         builtin cd $argv
+        set cd_status $status
         z --add (pwd)
     else
         z $argv
+        set cd_status $status
     end
+
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    return $cd_status
 end
 
 # Fuck steam locomotive
