b9d1ef520b565377c955fe23e5bdb78e5962dd1c
Dylan Johnston <dylanj@Dylans-MacBook.local>
2016-11-21 17:50:12 +0800

    Made installable on fresh computer

--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -o pipefail
 set -o nounset
-set -e
+set -o errexit
 
-IFS=$'\n'
+# set -x
+
+# cd /tmp
+# git clone https://github.com/DylanRJohnston/dotfiles.git
+# cd dotfiles
+
 DIR="$(cd "$(dirname "$0")"; pwd)"
-
-function error()
-{
-    #ANSI escape codes 5 blinking, 37 white text, 41 red background, 0 nothing.
-    printf "\e[5;37;41mError\e[0;37;41m: $@ \e[0m\n"
-}
-
-if [[ -z "${HOME}" ]]; then
-    error "HOME envrioment variable not set!"
-    exit 1
-fi
-
-for file in $(ls -1 "${DIR}" | grep -Ev "$(cat "${DIR}/.ignore" | paste -sd \| -)"); do
-
-    #TODO recursively merge directories like .ssh to not override keys
-    if [[ -d "${HOME}/.${file}" ]]; then
-        error "Cannot merge directory ${file}, please remove and/or manually merge"
-        continue
-    fi
-
-    ln -sf "${DIR}/${file}" "${HOME}/.${file}"
+IFS=$'\n'
+FILES=( $(<dotfiles/DOTFILES) )
+# echo "${DIR}"
+for FILE in ${FILES[@]}; do
+    FILE_PATH="${HOME}/.$FILE"
+    mkdir -p "$(dirname $FILE_PATH)" || true
+    ln -sf "${DIR}/dotfiles/${FILE}" "${FILE_PATH}"
 done
+
+./other/install.sh
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# error() {
+#     #ANSI escape codes 5 blinking, 37 white text, 41 red background, 0 nothing.
+#     printf "\e[5;37;41mError\e[0;37;41m: $@ \e[0m\n"
+# }
+#
+# if [[ -z "${HOME}" ]]; then
+#     error "HOME envrioment variable not set!"
+#     exit 1
+# fi
+#
+# for file in $(ls -1 "${DIR}" | grep -Ev "$(cat "${DIR}/.ignore" | paste -sd \| -)"); do
+#
+#     #TODO recursively merge directories like .ssh to not override keys
+#     if [[ -d "${HOME}/.${file}" ]]; then
+#         error "Cannot merge directory ${file}, please remove and/or manually merge"
+#         continue
+#     fi
+#
+#     ln -sf "${DIR}/${file}" "${HOME}/.${file}"
+# done
--- other/install.sh
+++ other/install.sh
@@ -8,13 +8,24 @@ readInFile() {
     < $1 grep -oE '^[^#]+' | tr '\n' ' '
 }
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew tap $(readInFile ./Tapfile)
 brew install $(readInFile ./Brewfile)
 brew cask install $(readInFile ./Caskfile)
-#npm i -g $(readInFile ./Nodefile)
 apm install $(readInFile ./Atomfile)
-
 
 for APP in $(readInFile ./Masfile); do
     mas install $APP
 done
+
+curl -L http://get.oh-my.fish | fish
+
+omf install agnoster
+omf install z-cd
+omf theme agnoster
+
+curl http://j.mp/spf13-vim3 -L -o - | sh
+vim +BundleInstall! +BundleClean +q
+
+./settings.sh
