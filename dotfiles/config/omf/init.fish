3e80828fc16bee8358400abecf801ba483e6bc4c
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2017-05-02 14:22:41 -0700

    Hard code $PATH

--- dotfiles/config/omf/init.fish
+++ dotfiles/config/omf/init.fish
@@ -162,14 +162,12 @@ function decrypt-audible
     )
     ffmpeg -activation_bytes $activation_bytes -i $argv[1] -vn -c:a copy (basename $argv[1] .aax).m4a
 end
+
 #
 # ENVIRONMENT VARIABLES
 #
-set -gx PATH $HOME/.local/bin $HOME/.config/yarn/global/node_modules/.bin /usr/local/sbin $PATH
-set -gx CC /usr/local/bin/gcc-6
-set -gx CXX /usr/local/bin/g++-6
-set -gx CPP /usr/local/bin/cpp-6
-set -gx LD  /usr/local/bin/gcc-6
+
+set -gx PATH $HOME/.local/bin $HOME/.config/yarn/global/node_modules/.bin /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
 set -gx HOMEBREW_MAKE_JOBS (sysctl -n hw.logicalcpu)
 
 set fish_greeting ""
